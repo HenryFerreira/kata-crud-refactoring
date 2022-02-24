d04484d252bd445aacec6440e2dd5cb37f25e4f8
HenryFerreira <henryferreira133@gmail.com>
2022-02-24 19:16:05 -0300

    Se creo el 'Form' para TodoList y se implemento el metodo para agregar elementos a la base de datos

--- front/src/App.js
+++ front/src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 import Form from './components/From';
 import List from './components/List';
 import StoreProvider from './components/StoreProvider';
-
+import FormTodoList from './components/FormTodoList';
 //---------------------------------------------------------------------//
 //Constantes
 const HOST_API = "http://localhost:8080/api";
@@ -17,6 +17,7 @@ function App() {
     <StoreProvider>
       <h3>To-Do List</h3>
       <Form />
+      <FormTodoList />
       <List />
     </StoreProvider>
   );
